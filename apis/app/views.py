2802776a144d979b6d2b88bce78ca34d65ad175e
Eric-Jiang1 <ericjiang0325@gmail.com>
2024-03-28 14:29:33 -0400

    added functionality, test installing pytorch on server

--- apis/app/views.py
+++ apis/app/views.py
@@ -4,29 +4,36 @@ from django.db import connection
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-def getchatts(request):
-    if request.method != 'GET':
-        return HttpResponse(status=404)
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+import numpy as np
+import base64
 
-    cursor = connection.cursor()
-    cursor.execute('SELECT username, message, time FROM chatts ORDER BY time DESC;')
-    rows = cursor.fetchall()
-
-    response = {}
-    response['chatts'] = rows
-    return JsonResponse(response)
 
 @csrf_exempt
-def postchatt(request):
+def speech_emotion_analysis(request):
+    # Process a base64 encoded string sent as part of a json object with key: "audio"
+    # csrf exempt since the certificate is self-signed 
     if request.method != 'POST':
         return HttpResponse(status=404)
 
     json_data = json.loads(request.body)
-    username = json_data['username']
-    message = json_data['message']
+    base64_audio_text = json_data['audio']
 
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO chatts (username, message) VALUES '
-                   '(%s, %s);', (username, message))
+    mapper = ["angry", "disgust", "fear", "happy",
+              "neutral", "other", "sad", "surprised", "unknown"]
+    inference_pipeline = pipeline(
+        task=Tasks.emotion_recognition,
+        model="iic/emotion2vec_base_finetuned", model_revision="v2.0.4")
+    audio_bytes = base64.b64decode(base64_audio_text)
 
-    return JsonResponse({})
+    rec_result = inference_pipeline(
+        audio_bytes, output_dir="./outputs", granularity="utterance", extract_embedding=False)
+    max_emotion_score = np.argmax(rec_result[0]["scores"])
+
+    results = {
+        "emotion": mapper[max_emotion_score],
+        "confidence":rec_result[0]["scores"][max_emotion_score]
+    }
+
+    return JsonResponse(results)
